5566dcc61a2d56914f5980bb1102baba50481600
ngodi <albertngodi@gmail.com>
2019-08-26 19:55:27 +0100

    devise for Users created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
     def index
     end 
+    def new
+     @user = User.new
+    end
+
+    def create
+    end
+
+    private
+
+    def user_params
+     params.require(:user).permit(:email,:password)
+    end
 end
