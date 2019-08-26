c8a75bbfc3f568a30d7a70a4000f2f3fe96c5a96
ngodi <albertngodi@gmail.com>
2019-08-26 22:23:39 +0100

    added tests for email, password validity

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Bootstrap 3
 gem 'bootstrap-sass'
+
 gem 'jquery-rails'
 gem 'devise'
 # Reduces boot times through caching; required in config/boot.rb
@@ -43,7 +44,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 3.6.0'
+  gem 'rspec-rails', '~> 3.8.2'
   gem 'webdrivers'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name" do 
+    user = User.new(name: "Diego", email: "diego@gmail.fr", password: "Hey12345678") 
+    expect(user).to be_valid 
+  end 
+
+  it "expects short passwords to be invalid" do
+    user = User.new(name: "Diego", email: "diego@gmail.fr", password: "He78") 
+    expect(user).not_to be_valid 
+  end
+
+  it "expects wrong email formats to be invalid" do
+    user = User.new(name: "Diego", email: "diego.gmail.fr", password: "Heey12345678") 
+    expect(user).not_to be_valid 
+  end
+
 end
