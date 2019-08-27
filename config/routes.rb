84d2ac22b376a3a8d18ae2d4181ad4f833f52196
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-27 14:55:03 -0500

    Sign in, out and sign up implementation with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do 
-    root to: "devise/sessions#new" 
-  end 
+  
   
 end
