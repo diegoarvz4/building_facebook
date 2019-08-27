4ae95a6c2952318e9135ad90149e41542e90aa8b
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-27 16:17:36 -0500

    Updated user config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 end
