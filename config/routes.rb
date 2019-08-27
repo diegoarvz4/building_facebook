35832ec89902ae8975e75d674d24bdd1d7697983
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-27 17:26:28 -0500

    user sessions and sign up basic test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 
-RSpec.describe 'Login a user', type: :feature do
+RSpec.describe 'User session test', type: :feature do
    # user = User.create!(name: 'Jon',email:"diego@email.com", password: "1234567890")
-    scenario 'login a user with valid name input' do
-      
+    scenario 'login a user with valid email and password input and then logout' do
+      user = User.create!(email: 'diego@email.com', password:'1234567890')
       visit new_user_session_path
       fill_in 'Email', with: 'diego@email.com'
       fill_in 'Password', with: '1234567890'
       click_on 'Log in'
-    #  visit new_user_session_path(user)
-    #  expect(page).to have_content('Log out')
+      expect(page).to have_content('Sign out')
+      click_on 'Sign out'
+      expect(page).to have_content('Sign in or Sign up')
     end
 
   
