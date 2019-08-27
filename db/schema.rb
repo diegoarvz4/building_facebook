051e277eca1cad60d8c03d076f78aa69c01625d9
diegoarvz4 <darvz.gtz@gmail.com>
2019-08-27 17:52:03 -0500

    Posts model creation and author id FK association with User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_182810) do
+ActiveRecord::Schema.define(version: 2019_08_27_224218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_08_26_182810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
