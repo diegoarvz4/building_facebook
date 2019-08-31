6574d53c73055011e3dcb1fe090f4ea731323cf4
ngodi <albertngodi@gmail.com>
2019-08-31 05:01:59 +0100

    overloaded the after_sign_out_path_for helper as a fix for undesired flashes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+ 
+  resources :users, only: [:index, :edit, :show, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  
+
   get 'posts/index'
   get 'posts/new'
   post 'posts/create'
@@ -6,21 +13,7 @@ Rails.application.routes.draw do
   get 'posts/edit'
   get 'posts/update'
   
-devise_scope :user do
+ 
 
-  root to: 'devise/sessions#new'
-
-end
-
-authenticated :user do
-
-  root 'pages#home'
-
-end
-
-  resources :users, only: [:index, :edit, :show, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
-
-  
+     
 end
