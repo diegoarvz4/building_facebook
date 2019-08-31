5fba26eec125738cad9c56c8e0597622dcdfa7b4
ngodi <albertngodi@gmail.com>
2019-08-31 02:02:18 +0100

    added root route helpers for authenticated users and removed redundant views

--- app/helpers/devise_helpers.rb
+++ app/helpers/devise_helpers.rb
@@ -1,4 +1,6 @@
 module DeviseHelper
+ 
+
     def devise_error_messages!
       return '' if resource.errors.empty?
   
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,22 @@ Rails.application.routes.draw do
   delete 'posts/destroy'
   get 'posts/edit'
   get 'posts/update'
-  root to: 'pages#home'
+  
+devise_scope :user do
+
+  root to: 'devise/sessions#new'
+
+end
+
+authenticated :user do
+
+  root 'pages#home'
+
+end
 
   resources :users, only: [:index, :edit, :show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
+  
 end
