4625196fed3cdbe269b38add4050fb3877a47a4e
ngodi <albertngodi@gmail.com>
2019-09-03 17:06:15 +0100

    comment display in descending order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  default_scope -> { order(created_at: :desc) }
 
   validates :content, presence: true
 end
