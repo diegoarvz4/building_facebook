6defb69456bfa4e0973dc19cb9d3c702a545a444
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-11 12:22:30 -0500

    added friendship tests

---
+++ spec/features/basic_friendship_rspec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal:true
+
+require 'rails_helper'
+
+RSpec.describe 'User friendship test', type: :feature do
+  scenario 'login a user and send a friend request. Then the friend accepts the request' do
+    User.create!(email: 'diego@email.com', password: '1234567890')
+    User.create!(email: 'alberto@email.com', password: '1234567890')
+    visit new_user_session_path
+    fill_in 'Email', with: 'diego@email.com'
+    fill_in 'Password', with: '1234567890'
+    click_on 'Log in'
+    expect(page).to have_content('Sign out')
+    click_on 'Users'
+    click_on 'Add friend'
+    expect(page).to have_content('Friend request sent')
+    expect(page).to have_content('Pending')
+    click_on 'Sign out'
+    expect(page).to have_content('Signed out successfully')
+    fill_in 'Email', with: 'alberto@email.com'
+    fill_in 'Password', with: '1234567890'
+    click_on 'Log in'
+    expect(page).to have_content('1 pending friend request')
+    click_on '1 pending friend request'
+    expect(page).to have_content('accept')
+    click_on 'accept'
+    expect(page).to have_content('Friend accept')
+  end
+end
