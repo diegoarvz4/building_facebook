eb6e7a58469b7a96db8fa0a657ae10ea5051c5c0
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-11 13:26:45 -0500

    Mutual friendships

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
   def current_user?(user)
     user == current_user
   end
+
+  def friendship(user)
+    Friendship.find_by(user_id: user.id, friend_id: current_user.id)
+  end
 end
