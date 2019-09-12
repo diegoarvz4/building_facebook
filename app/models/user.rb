12635adaa03dc341c643c4eb5394f67976a4fb37
ngodi <albertngodi@gmail.com>
2019-09-12 23:30:38 +0100

    linting errors fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,20 +20,20 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      if data
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.image = auth.info.image # assuming the user model has an image
     end
   end
-
 
   private
 
