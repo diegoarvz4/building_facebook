8a4281ce5322f0e684ba74e19220d518dbf290e4
ngodi <albertngodi@gmail.com>
2019-09-12 21:16:12 +0100

    omniauth setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,4 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.scoped_views = true
+
+  config.omniauth :facebook, "229696334651787", "0d26cc5d0b16304a10786d28939decac", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
  
   post '/friendship', to: 'users#request_friend'
   patch '/friendship', to: 'users#accept_friend'
+  get '/users/auth/facebook/callback', to: redirect('/auth/facebook/callback')
   resources :users, only: [:index, :show]
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :comments
   resources :likes, only: [:create]
   resources :posts
