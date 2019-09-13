c5116bfa72367494e678806e5fc490bcfcc30005
ngodi <albertngodi@gmail.com>
2019-09-13 23:00:10 +0100

    password request on user edit removed

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   patch '/friendship', to: 'users#accept_friend'
   get '/users/auth/facebook/callback', to: redirect('/auth/facebook/callback')
   resources :users, only: [:index, :show]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'registrations' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :comments
   resources :likes, only: [:create]
   resources :posts
