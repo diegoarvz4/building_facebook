ae7474140bbef06510dd3ffb3235e65c278c4102
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-13 17:12:06 -0500

    readme file

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
-class RegistrationsController < Devise::RegistrationsController
+# frozen_string_literal: true
 
+class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
