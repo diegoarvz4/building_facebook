933e5c609eac9e85e8ac731a0fc6a49cc6183cad
diegoarvz4 <darvz.gtz@gmail.com>
2019-09-17 13:42:13 -0500

    move functions from users helper to friends module in lib folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 
 class ApplicationController < ActionController::Base
   include DeviseHelper
-  include UsersHelper
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  include Friends
   before_action :find_post, only: %i[show edit update destroy]
 
   def index
